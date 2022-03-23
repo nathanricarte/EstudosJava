0ddd5c682e522840140e179515de1581f51f20d8
Nathan Ricarte <61098505+nathanricarte@users.noreply.github.com>
2022-03-23 02:44:53 -0300

    Update ListaEstatica.java

--- ListaEstatica.java
+++ ListaEstatica.java
@@ -1,11 +1,53 @@
 public class ListaEstatica {
-	public void add(int elemento) {
-		
+	private int vetor[];
+	private int cont;
+	
+	public ListaEstatica(int n) {
+		vetor = new int[n];
+		cont = 0;
 	}
+	
 	public void add(int elemento) {
-		
+		if(cont < vetor.length) {
+			vetor[cont] = elemento;
+			cont++;
+		} else 
+			System.out.println("Lista cheia!");
 	}
+	
+	public void add(int posicao, int elemento) {  
+		if(posicao < vetor.length) {
+			for (int i = cont; i > posicao; i--)
+				vetor[i] = vetor[i - 1];
+		} else 
+			System.out.println("Posicao informada nao existe!");
+	}
+	
 	public void remove(int elemento) {
 		
+	}
+	
+	public int search(int posicao) {
+		return vetor[posicao];
+	}
+	
+	public boolean contain(int elemento) {
+		return false;
+	}
+	
+	public int size() {
+		return cont;
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public void show() {
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+		System.out.println();
+			
 	}
 }
